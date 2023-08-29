b8857a18d66ff7bd7fe4d795469650edbd13add9
Danny Thottumkara <danish12301@gmail.com>
2023-08-29 19:39:26 +0000

    Exercise thru 3.8

--- exercise/ex01/application/src/main/java/com/fenago/application/Application.java
+++ exercise/ex01/application/src/main/java/com/fenago/application/Application.java
@@ -6,6 +6,7 @@ import java.util.stream.Stream;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
